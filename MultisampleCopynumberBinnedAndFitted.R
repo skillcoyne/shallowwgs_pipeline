d5b6b8e339e489e50a6710f6d026fdf4e749bb41
Sarah Killcoyne <sk875@mrc-cu.cam.ac.uk>
2016-09-30 13:07:40 +0100

    Updated to use ggplot, and include new data.  Still some hardcoding as the data format has changed.

--- MultisampleCopynumberBinnedAndFitted.R
+++ MultisampleCopynumberBinnedAndFitted.R
@@ -1,14 +1,18 @@
 library(plyr)
+library(ggplot2)
+library(ggdendro)
 # this version removes qDNAseq 'blacklisted' regions and uses 'fitted' read counts
 
-setwd("/lustre/scratch112/sanger/cgppipe/PanCancerDownloads/workspace/dw9/Ellie")
+#setwd("/lustre/scratch112/sanger/cgppipe/PanCancerDownloads/workspace/dw9/Ellie")
 
-setwd("~/Documents/MRC-OwnCloud/skillcoyne/Documents/Ellie BE Project/")
+#setwd("~/Documents/MRC-OwnCloud/skillcoyne/Documents/Ellie BE Project/")
 
 data = "~/Data/Ellie/QDNAseq"
-data.dirs = paste(data, c("SLX-10725_10729","SLX-9242","SLX-9246"), sep="/")
 
-outdir = "plots_fitted"
+data.dirs = list.dirs(data, full.names=T, recursive=F)
+#data.dirs = paste(data, c("SLX-10725_10729","SLX-9242","SLX-9246"), sep="/")
+
+outdir = "~/tmp/plots_fitted"
 if ( !dir.exists(outdir) ) 
   dir.create(outdir)
 
@@ -16,90 +20,100 @@ if ( !dir.exists(outdir) )
 fit.data = NULL
 raw.data = NULL
 samplenames=NULL
-for(dir in data.dirs){
+for(dir in data.dirs) {
   print(dir)
-	files = list.files(dir,pattern="fittedReadCounts.txt")
-	short.name = gsub("_fitted","",gsub("^.*/SLX-","",dir))
-	for(file in files){
-		print(file)
-		spl = strsplit(file,"\\.")
-		
-		fitted = paste(dir,"/",file,sep="")
-		raw = paste(dir,"/",gsub("fitted","raw",file),sep="")
-		if (!file.exists(fitted)) {
-		  stop("Missing fitted file ")
-		}
-		if (!file.exists(raw))
-		  stop("Missing raw file")
-		  
-		next
-		
-		if(is.null(fit.data)){
-			fit.data = read.table(paste(dir,"/",file,sep=""),sep="\t",header=T,stringsAsFactors=F)  # fitted reads
-			samplenames = paste(spl[[1]][2],short.name,sep="_")
-			raw.data = read.table(paste(dir,"/",gsub("fitted","raw",file),sep=""),sep="\t",header=T,stringsAsFactors=F) # raw reads
-		}else{ # add to table
-			fit.data = cbind(fit.data,(read.table(paste(dir,"/",file,sep=""),sep="\t",header=T,stringsAsFactors=F))[,5])
-			samplenames = c(samplenames,paste(spl[[1]][2],short.name,sep="_"))
-			raw.data = cbind(raw.data,(read.table(paste(dir,"/",gsub("fitted","raw",file),sep=""),sep="\t",header=T,stringsAsFactors=F))[,5])
-		}
-	}
-}
-names(fit.data)[-(1:4)] = samplenames
-names(raw.data)[-(1:4)] = samplenames
-save.image(file="MultisampleCopynumberBinnedAndFitted1.RData")
+	files = list.files(dir,pattern="[fitted|corrected]ReadCounts.txt")
 
-depth.cols = (1:ncol(fit.data))[-(1:4)] # "count" columns
-no.samples = length(samplenames)
-# adjusting the raw counts by the fitted counts (need to find out what "fitted" is in this context)
-window.depths = raw.data[,depth.cols]/fit.data[,depth.cols] 
+	if (length(files) == 1) {
+	  fit = read.table(paste(dir,"/",files[1],sep=""),sep="\t",header=T,stringsAsFactors=F)  # fitted reads
+	  cols = grep('D\\d', colnames(fit))
+	  colnames(fit)[cols]= paste(colnames(fit)[cols], '10722_11823', sep="_") # hardcoded...fix this from spreadsheets later
+	  
+	  raw = read.table(paste(dir,"/",gsub("fitted|correctedR","r",files[1]),sep=""),sep="\t",header=T,stringsAsFactors=F) # raw reads
+	  colnames(raw)[cols]= paste(colnames(raw)[cols], '10722_11823', sep="_") # hardcoded...fix this from spreadsheets later
+	  
+	  if (is.null(fit.data)) {
+	    fit.data = fit
+	    raw.data = raw
+	  } else {
+	    fit.data = cbind(fit.data,fit[,cols])
+	    raw.data = cbind(raw.data,raw[,cols])
+	  }
+
+	} else {
+  	short.name = gsub("_fitted","",gsub("^.*/SLX-","",dir))
+	  for(file in files){
+		  print(file)
+		  spl = strsplit(file,"\\.")
+		
+  		fitted.file = paste(dir,"/",file,sep="")
+  		raw.file = paste(dir,"/",gsub("fitted","raw",file),sep="")
+  		if (!file.exists(fitted.file) | !file.exists(raw.file)) 
+  		  stop("Missing fitted/raw file ")
+
+  		fit = read.table(fitted.file,sep="\t",header=T,stringsAsFactors=F)  # fitted reads
+  		colnames(fit)[5] = paste(spl[[1]][2],short.name,sep="_")
+  		
+  		raw = read.table(raw.file,sep="\t",header=T,stringsAsFactors=F) # raw reads
+  		colnames(raw)[5] = paste(spl[[1]][2],short.name,sep="_")
+
+	  	if(is.null(fit.data)){
+	  		fit.data = fit
+	  		raw.data = raw
+	  	}else{ # add to table
+	  		fit.data = cbind(fit.data,fit[,5])
+	  		raw.data = cbind(raw.data, raw[,5])
+	  		
+	  		colnames(fit.data)[ncol(fit.data)] = colnames(fit)[5]
+	  		colnames(raw.data)[ncol(raw.data)] = colnames(raw)[5]
+	  	}	  
+	  }
+  }
+}
+save.image(file=paste(data,"MultisampleCopynumberBinnedAndFitted1.RData", sep='/'))
 
 
 chrs = ddply(fit.data, .(chrom), summarise, length=max(end))
 chrs$chrom = factor(chrs$chrom, c((1:22),"X","Y","M"), ordered=T)
 chrs = chrs[order(chrs$chrom),]
 chrs$cum.lengths = cumsum(as.numeric(chrs$length))
+for (c in 1:nrow(chrs)) {
+  chrs[c,'l.pos'] = ifelse(c==1, as.numeric(chrs$cum.lengths[c]/2), as.numeric((chrs$cum.lengths[c-1]+chrs$cum.lengths[c])/2))
+}
 
-# chrs = unique(fit.data$chrom)
-# chr.lengths = vector(length = length(chrs),mode="integer")
-# for(c in 1:length(chrs)){
-# 	chr = chrs[c]
-# 	chr.lengths[c] = max(fit.data$end[fit.data$chrom==chr])
-# }
-
-#cum.lengths = cumsum(as.numeric(chr.lengths))
-fit.data$genome.pos = as.numeric(fit.data$start)
 # moving the position information from chromosomes to the genome position (cumulutive sum of the lengths of the chrs)
-#for(c in 2:length(chr.lengths)){
+fit.data$genome.pos = as.numeric(fit.data$start)
 for (c in 2:nrow(chrs)) {
-	fit.data$genome.pos[fit.data$chrom == chrs[c, 'chrom']] = fit.data$genome.pos[fit.data$chrom == chrs[c, 'chrom']] + chrs[c-1, 'cum.lengths']   #cum.lengths[c-1]
+  fit.data$genome.pos[fit.data$chrom == chrs[c, 'chrom']] = fit.data$genome.pos[fit.data$chrom == chrs[c, 'chrom']] + chrs[c-1, 'cum.lengths']   #cum.lengths[c-1]
+}
+save.image(file=paste(data,"MultisampleCopynumberBinnedAndFitted2.RData", sep='/'))
+
+# "count" columns
+depth.cols = grep('^D\\d',colnames(fit.data))
+no.samples = length(depth.cols)
+# adjusting the raw counts by the fitted counts (need to find out what "fitted" is in this context)
+window.depths = raw.data[,depth.cols]/fit.data[,depth.cols] 
+window.depths = cbind(fit.data[,c('chrom','start','end','genome.pos')], window.depths)
+
+samplenames = colnames(fit.data)[depth.cols]
+chr.info = colnames(window.depths)[1:depth.cols[1]-1]
+for (s in samplenames) {
+  print(paste("plotting ",s,sep=""))
+  png(paste(outdir,paste(s,"coverage_binned_fitted.png",sep="_"),sep="/"),width=1600,height=800)
+  gg = ggplot(window.depths[,c(chr.info, s)],aes_string(x="genome.pos", y=s)) + lims(y=c(-0.25,3), x =c(0,max(window.depths$genome.pos))) + 
+    geom_point(color='darkred', alpha=0.5) +
+    geom_vline(data=chrs, xintercept=c(0,chrs$cum.lengths), color='darkgrey') + 
+    geom_text(data=chrs, aes(x=l.pos, y=rep(3, length(chrom)), label=chrom)) + 
+    ggtitle(s) + labs(x='genome pos', y="corrected depth/15KB")
+  print(gg)
+  dev.off()
 }
 
-save.image(file="MultisampleCopynumberBinnedAndFitted2.RData")
+hc = hclust(dist(t( window.depths[depth.cols] )))
+ggd = ggdendrogram(hc)
 
-png(paste(outdir,"/coverage_binned_fitted.png",sep=""),width=24000,height=4000)
-par(mfrow=c(12,10))
-for(s in 1:length(samplenames)){
-	print(paste("plotting ",samplenames[s],sep=""))
-	plot(fit.data$genome.pos,window.depths[,s],pch=20,col="red",cex=0.5,xlab="",ylab="corrected depth/15KB",xaxt="n",main=samplenames[s],ylim=c(0,3))
-	abline(v=0,lty=1,col="lightgrey",lwd=2)
-	for(c in chrs){
-	  abline(v=chrs[c, 'cum.lengths'],lty=1,col="lightgrey",lwd=2)
-		#abline(v=cum.lengths[c],lty=1,col="lightgrey",lwd=2)
-		if(c==1){
-			text(cum.lengths[c]/2,3,chrs[c],pos=1,cex=2)
-		}else{
-			text((cum.lengths[c-1]+cum.lengths[c])/2,3,chrs[c],pos=1,cex=2)
-		}
-	}
-}
-dev.off()
-
-hclust.data = window.depths
-names(hclust.data)=samplenames
-HC = hclust(dist(t(hclust.data)))
-png(paste(outdir,"/hierarchical_clustering_plot_CNA_binned_fitted.png",sep=""),width=2400)
-plot(HC,xlab="",main="",sub="")
+png(paste(outdir,"/hierarchical_clustering_plot_CNA_binned_fitted.png",sep=""),width=2400, height=1600)
+print(ggd)
 dev.off()
 
 q(save="no")
