50ccbce3a2d9fc40ded38c8933b936b16ee37b29
Sarah Killcoyne <sk875@mrc-cu.cam.ac.uk>
2016-12-01 12:17:22 +0000

    Mapping genes from the cosmic gene consensus onto the variable regions.

--- lib/load_patient_metadata.R
+++ lib/load_patient_metadata.R
@@ -11,7 +11,7 @@ read.patient.info<-function(file) {
   for (i in 1:length(getSheets(loadWorkbook(file)))) {
     print(i)
     ws = read.xlsx2(file, sheetIndex=i, stringsAsFactors=F, header=T)
-    ws = ws[which( ws$Patient.ID != ""), grep('Patient|Endoscopy.Year|Pathology$|Progressor|Plate.Index|SLX|cellularity', colnames(ws), value=T, ignore.case=T)]
+    ws = ws[which( ws$Patient.ID != ""), grep('Patient|Path\\.ID|Endoscopy.Year|Pathology$|Progressor|Plate.Index|SLX|cellularity', colnames(ws), value=T, ignore.case=T)]
     head(ws)
     #head(patient.info)
     
@@ -24,6 +24,7 @@ read.patient.info<-function(file) {
     
     ## Order columns
     ws = ws[,c(grep('Patient',colnames(ws), ignore.case=T),
+               grep('Path\\.ID',colnames(ws), ignore.case=T),
                grep('Progressor',colnames(ws), ignore.case=T),
                grep('Endoscopy.year',colnames(ws), ignore.case=T),
                grep('Pathology',colnames(ws),ignore.case=T),
@@ -31,7 +32,7 @@ read.patient.info<-function(file) {
                grep('SLX',colnames(ws),ignore.case=T),
                grep('cellularity',colnames(ws),ignore.case=T)) ]
     
-    colnames(ws) = c('Patient','Status','Endoscopy.Year','Pathology','Plate.Index','SLX.ID','Barretts.Cellularity')
+    colnames(ws) = c('Patient','Path.ID','Status','Endoscopy.Year','Pathology','Plate.Index','SLX.ID','Barretts.Cellularity')
     
     if (is.null(patient.info)) {
       patient.info = ws
@@ -40,13 +41,19 @@ read.patient.info<-function(file) {
     }
   }
   patient.info$SLX.ID = gsub('SLX-', '', strip.whitespace( patient.info$SLX.ID ) )
-  
+
   patient.info$Status = factor(strip.whitespace(patient.info$Status))
   patient.info$Pathology = factor(strip.whitespace(patient.info$Pathology))
   patient.info$Endoscopy.Year = as.numeric(patient.info$Endoscopy.Year)
   patient.info$Plate.Index = strip.whitespace(patient.info$Plate.Index)
   patient.info$Barretts.Cellularity = as.integer(patient.info$Barretts.Cellularity)
   patient.info$Samplename = gsub('-', '_', paste(patient.info$Plate.Index,strip.whitespace(patient.info$SLX.ID),sep="_"))
+  ## TODO Mistake in earlier version of the patient file caused this will be fixed for the next run
+  snames = grep('_1072(5|9)$', patient.info$Samplename)
+  if ( length(snames > 0) ) {
+    patient.info$Samplename[snames] =  paste( sub('-','_', patient.info$Plate.Index[snames]), '10725_10729', sep='_' )
+  }
+  
   patient.info = arrange(patient.info, Status, Patient, Endoscopy.Year)
 
   return(patient.info)
--- output_variable_regions.R
+++ output_variable_regions.R
@@ -1,11 +1,12 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
-#if (length(args) < 3) {
-#  stop("Data directory, patient name, and plot output directory required as arguments")
-#}
+if (length(args) < 3) {
+  stop("Missing required arguments: <data directory> <output directory> <cosmic tsv gene file>")
+}
 
 library(xlsx) 
+library(GenomicRanges)
 
 source("lib/fastPCF.R")
 source('lib/load_patient_metadata.R')
@@ -13,20 +14,40 @@ source('lib/load_patient_metadata.R')
 Negate('%in%') -> `%nin%`
 
 data = args[1]
-gamma2 = args[2]
-outdir = args[3]
+outdir = args[2]
+cosmic = args[3]
 
-data = '~/Data/Ellie/Analysis'
 gamma2 = 250
-outdir = "~/Documents/MRC-OwnCloud/Progressor Study/Variable Regions"
+if (length(args) == 4)
+  gamma2 = args[4]
+
+
+#data = '~/Data/Ellie/Analysis'
+#gamma2 = 250
+#outdir = "~/Documents/MRC-OwnCloud/Progressor Study/Variable Regions"
+# cosmic = '~/Data/CosmicCensusGenes.tsv'
 
 plot.dir = paste(data, 'multipcf_plots_fitted_perPatient', sep='/')
 print(paste("Plot directory:", plot.dir))
 if (!dir.exists(plot.dir))  
   stop("multipcf needs to be run first")
 
+ccgenes = read.table(cosmic, sep='\t', header=T, stringsAsFactors=F)
+
+noLoc = grep(':-',ccgenes$Genome.Location)
+noLocGenes = ccgenes[noLoc,]
+ccgenes = ccgenes[-noLoc,]
+
+locs = as.data.frame(do.call(rbind, strsplit(ccgenes$Genome.Location, ':|-')))
+colnames(locs) = c('chr','start','end')
+locs[c('start','end')] = lapply(locs[c('start','end')], function(x) as.numeric(as.character(x)))
+
+ccgenes = cbind(locs, ccgenes)
+ccgenes = makeGRangesFromDataFrame(ccgenes, keep.extra.columns = T, start.field = 'start', end.field = 'end')
+
+
 ## Patient info file
-patient.file = grep('patient_info.xlsx', data.files, value=T)
+patient.file = '~/Data/Ellie/QDNAseq/All_patient_info.xlsx'
 if (length(patient.file) != 1)
   stop(paste("Missing patient info file in", data))
 
@@ -35,20 +56,34 @@ all.patient.info = arrange(all.patient.info, Status, Patient, Endoscopy.Year)
 all.patient.info$Patient = gsub("/", "_", all.patient.info$Patient)
 head(all.patient.info)
 ## TODO Mistake in earlier version of the patient file caused this will be fixed for the next run
-snames = grep('_1072(5|9)$', all.patient.info$Samplename)
-if ( length(snames > 0) ) {
-  all.patient.info$Samplename[snames] =  paste( sub('-','_', all.patient.info$Plate.Index[snames]), '10725_10729', sep='_' )
-}
+# snames = grep('_1072(5|9)$', all.patient.info$Samplename)
+# if ( length(snames > 0) ) {
+#   all.patient.info$Samplename[snames] =  paste( sub('-','_', all.patient.info$Plate.Index[snames]), '10725_10729', sep='_' )
+# }
 
 ## Thresholds
-sd.threshold = 0.08 
-min.probes = 67  # this is what's used in the plotting script...
+sd.threshold = 0.08
+# this is what's used in the plotting script...
+min.probes = 67  
 
 gain.threshold = 1.1
 loss.threshold = 0.9
 ## ------- ##
 
-filename = path.expand( paste(outdir, "variable_regions.xlsx", sep='/') )
+#filename = path.expand( paste(outdir, "variable_regions.xlsx", sep='/') )
+
+cancer.consensus.genes<-function(regions, ccgenes) {
+  reg = as_tibble(regions)
+  gr = GenomicRanges::makeGRangesFromDataFrame(regions, start.field = 'start.pos', end.field = 'end.pos')
+
+  ov = findOverlaps(ccgenes, gr)
+  for (hit in unique(subjectHits(ov))) {
+    reg[hit, 'Gene.Symbols'] =  paste(ccgenes[queryHits(ov)[which(subjectHits(ov) == hit)],]$Gene.Symbol, collapse=',')
+  }
+  
+  regions = cbind(reg, regions[,-(1:5)])
+  return(regions)
+}
 
 appendFile = F
 for (patient.name in unique(all.patient.info$Patient)) {
@@ -67,11 +102,19 @@ for (patient.name in unique(all.patient.info$Patient)) {
   means = apply(segvals[,-(1:5)],1,mean)
   CofV = sds/means
   
+  cg = cancer.consensus.genes(segvals[,(1:5)], ccgenes)
+  segvals = cbind(cg$Gene.Symbols, segvals)
+  colnames(segvals) = sub('cg\\$', '', colnames(segvals) )
+  segvals = segvals[,c(2:6,1,7:ncol(segvals))] 
+  
   variable.region.indices = which(sds >= sd.threshold & segvals$n.probes >= min.probes)
 
   variable.regions = segvals[variable.region.indices, patient.info$Samplename]
   colnames(variable.regions) = paste(paste(patient.info$'Endoscopy.Year', patient.info$'Pathology', sep='-'), " (",patient.info$Plate.Index, ")", sep='')
-  #filename = paste(outdir, "/", patient.name, "_variable_regions_gamma", gamma2, '.txt', sep='')  
+  
+  head(segvals)
+  
+  filename = paste(outdir, "/", patient.name, "_variable_regions_gamma", gamma2, '.txt', sep='')  
   print(paste("variable regions:", nrow(variable.regions)))
 
   if (nrow(variable.regions) > 0) {
@@ -82,13 +125,15 @@ for (patient.name in unique(all.patient.info$Patient)) {
       gain_loss[i,which(variable.regions[i,] > loss.threshold & variable.regions[i,] < gain.threshold)] = 'NEUTRAL'
     }
     
-    write.xlsx2(cbind(segvals[variable.region.indices,c(1:5)], rbind( variable.regions, gain_loss ) ), file=filename, sheetName=patient.name, row.names=F, 
-                append=appendFile)
+    # write.xlsx2(cbind(segvals[variable.region.indices,c(1:5)], rbind( variable.regions, gain_loss ) ), file=filename, sheetName=patient.name, row.names=F, 
+    #             append=appendFile)
+    # 
     
-    #write.table(cbind(segvals[variable.region.indices,c(1:5)], rbind( variable.regions, gain_loss ) ), sep='\t', quote=F, row.names=F, file=filename, append = F)
+    write.table(cbind(segvals[variable.region.indices,c(1:6)], rbind( variable.regions ) ), sep='\t', quote=T, row.names=F, file=filename, append = F)
   } else {
-    write.xlsx2(cbind(segvals[variable.region.indices,c(1:5)],  variable.regions ), file=filename, sheetName=patient.name, row.names=F, append=appendFile)
-    #write.table(cbind(segvals[variable.region.indices,c(1:5)],  variable.regions ), sep='\t', quote=F, row.names=F, file=filename, append = F)
+    #write.xlsx2(cbind(segvals[variable.region.indices,c(1:5)],  variable.regions ), file=filename, sheetName=patient.name, row.names=F, append=appendFile)
+    
+    write.table(cbind(segvals[variable.region.indices,c(1:6)],  variable.regions ), sep='\t', quote=F, row.names=F, file=filename, append = F)
   }
   
   # sink(filename, append=T)
@@ -99,7 +144,7 @@ for (patient.name in unique(all.patient.info$Patient)) {
   appendFile = T
 }
 
-write.xlsx2(cbind( 'gamma2'=gamma2, 'sd'=sd.threshold, 'min.probes'=min.probes, 'gain'=gain.threshold, 'loss'=loss.threshold), filename, sheetName='thresholds',row.names=F, append=T)
+#write.xlsx2(cbind( 'gamma2'=gamma2, 'sd'=sd.threshold, 'min.probes'=min.probes, 'gain'=gain.threshold, 'loss'=loss.threshold), filename, sheetName='thresholds',row.names=F, append=T)
 
 
 
