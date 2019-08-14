2ecf69977e610ff3357ed620d07da58f41028946
Sarah Killcoyne <sk875@mrc-cu.cam.ac.uk>
2019-08-14 13:31:26 +0100

    ...

--- processing_pipeline/predict_downsampled.R
+++ processing_pipeline/predict_downsampled.R
@@ -47,24 +47,13 @@ dir.create(pred.dir, showWarnings = F, recursive = T)
   
 segObj = BarrettsProgressionRisk::segmentRawData(info, rawFile, fittedFile, cutoff=0.011)
   
+p1 = BarrettsProgressionRisk::plotSegmentData(segObj) + labs(title=sample)
+
+ggsave(paste0(pred.dir, '/', sample, '_segmentedCoverage.png'), plot=p1,  width=20, height=6, units='in', limitsize=F)
+
 prr = BarrettsProgressionRisk::predictRiskFromSegments(segObj, model = fit, s = s)
-  
+
 save(prr, file=paste0(pred.dir, '/predictions.Rdata'))
-
-#save(dspred, all.preds, file=paste(model.dir, 'downsampled-predictions.Rdata', sep='/'))
-
-# dspred$Sample = rownames(dspred)
-# 
-# y = c(0.3,0.5,0.8)
-# names(y) = c('Low','Moderate','High')
-# dspred$y = y[dspred$Risk]
-# 
-# pcal = showPredictionCalibration() + 
-#   geom_point(data=dspred, aes(Prob,y), color='grey39', shape=18, size=5, show.legend = F) + 
-#   geom_point(data=dspred, aes(Prob,y,color=Risk), shape=18, size=4, show.legend=F) +
-#   geom_text_repel(data=dspred, aes(Prob,y,label=Sample,angle=45), show.legend=F )
-# 
-# ggsave(filename=paste(outdir, 'predictions_plot.png',sep='/'), plot=pcal, width=10, height=10, units='in')
 
 print("Finished'")
 
